6a7d2d2b0daffcefb83a6a5515665632fbdf1f67
Alisson Fernandes de Paula <alisson.paula@valecard.com.br>
2025-02-10 15:06:31 -0300

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     allowedHosts: [
       'all',
       'c159f961-59e9-4135-86ef-d42a904db6f4.lovableproject.com', // Adicione o host aqui
+      '5a71e751-82c7-4d34-b019-ab3b40def8bf.lovableproject.com',
     ],
   },
   plugins: [
